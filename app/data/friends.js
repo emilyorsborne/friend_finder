4dbb16f91ac9d64a9ab28054f2ea48c89a6b54b5
emilyorsborne <emorsborne@gmail.com>
2017-11-10 22:47:22 -0500

    draft1

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [{
+    name: 'Anthony Joshua',
+    photo: 'http://thenet.ng/wp-content/uploads/2017/05/anthony-joshua.jpg',
+    scores: [2, 5, 1, 3, 1, 3, 5, 4, 1, 5]
+}, {
+    name: 'Bradley Beal',
+    photo: 'https://img.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/10/29/Weekend/Images/beal031350506778.jpg',
+    scores: [3, 2, 4, 4, 5, 3, 1, 2, 4, 1]
+}, {
+    name: 'Julie Ertz',
+    photo: 'https://peopledotcom.files.wordpress.com/2016/08/julie-johnston-800.jpg?w=800',
+    scores: [2, 3, 4, 2, 3, 3, 1, 4, 3, 3]
+}, {
+    name: 'Charlotte Caslick',
+    photo: 'https://www.fairfaxstatic.com.au/content/dam/images/g/h/b/u/s/2/image.related.articleLeadNarrow.300x0.ghbjz4.png/1432798110740.jpg',
+    scores: [2, 1, 3, 1, 3, 5, 4, 3, 2, 2]
+}];
+
+module.exports = friends;
